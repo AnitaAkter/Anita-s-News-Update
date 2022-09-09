53dab4f63af2bfba097d669f5969fa4684fa45fc
AnitaAkter <108604188+AnitaAkter@users.noreply.github.com>
2022-09-09 23:47:12 +0600

    third-commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Categories code.......///
+const allCategories = () => {
+    fetch(`https://openapi.programming-hero.com/api/news/categories`)
+        .then(response => response.json())
+        .then(data => categoryList(data.data.news_category))
+}
+
+const categoryList = (lists) => {
+    const allCategories = document.getElementById('category');
+    lists.forEach(list => {
+        const { category_name, category_id } = list;
+        const createLi = document.createElement('li');
+        createLi.innerHTML = `
+          <li class="bg-secondary  px-3 py-3 me-3 rounded">
+          <a class="active text-white  fs-5 text" aria-current="page" onclick="thumbnailDetails('${category_id}')" href="#" style="text-decoration:none">${category_name}</a>
+          </li>
+          `
+        allCategories.appendChild(createLi);
+    }
+    )
+}
+allCategories();
