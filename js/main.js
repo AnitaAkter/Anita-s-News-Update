779651ef6ad6385e001c9f258cad32c22d15326e
AnitaAkter <108604188+AnitaAkter@users.noreply.github.com>
2022-09-10 00:33:03 +0600

    fifth-commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// Categories code.......///
+//.......... Categories Js.......///
 const allCategories = () => {
     fetch(`https://openapi.programming-hero.com/api/news/categories`)
-        .then(response => response.json())
+        .then(res => res.json())
         .then(data => categoryList(data.data.news_category))
 }
 
@@ -20,7 +20,102 @@ const categoryList = (lists) => {
     )
 }
 
+//..........Thumbnail's Js.........// 
 
+const thumbnailDetails = (category_id) => {
+    spinnerLoading(true)
+
+    fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+        .then(res => res.json())
+        .then((data) => {
+            spinnerLoading(false)
+            let localData = data.data
+
+            localData.sort((a, b) => b.total_view - a.total_view)
+
+
+            allThumbnailDetails(localData);
+        })
+        .catch((error) => {
+            spinnerLoading(false)
+            console.log(error)
+        })
+}
+
+
+const spinnerLoading = isLoading => {
+    const spinner = document.getElementById("spinner");
+    if (isLoading) {
+        spinner.classList.remove('d-none')
+    } else {
+        spinner.classList.add('d-none')
+    }
+}
+
+const allThumbnailDetails = (allInfo) => {
+
+    const thumbnailDetails = document.getElementById('thumbnail-info');
+    const searchedItem = document.getElementById('searched-item')
+    const searchedresult = allInfo.length;
+    if (searchedresult <= 0) {
+        searchedItem.innerText = 'No Data found'
+    }
+    else {
+        searchedItem.innerText = searchedresult + '' + 'Result founded'
+    }
+
+    thumbnailDetails.textContent = '';
+    allInfo.forEach(data => {
+        const { title, image_url, details, author, total_view, _id } = data;
+        const { img, name, published_date } = author;
+        const createDiv = document.createElement('div');
+        createDiv.classList.add('col');
+        createDiv.classList.add('h-100');
+        createDiv.innerHTML = `
+          <div class="card mb-3 rounded border-none" style="width: 100%; height: 200px;">
+          <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${image_url}" class="rounded p-2" alt="..." style="width: 100%; height: 200px;">
+        </div>
+        <div class="col-md-8">
+        <div class="card-body">
+        <h5 class="card-title">${title.length > 40 ? title.slice(0, 50) + '...' : title}</h5>
+            <p class="card-text">${details.length > 100 ? details.slice(0, 100) + '...' : details}</p>
+            <div class="card-footer mt-5">
+            <small class=" d-flex">
+            <div class="container">
+              <div class="row">
+                  <div class="col">
+                  <div class="d-flex">
+                      <img src="${img}" class="rounded" alt="..." style="width: 30px; height: 30px; ">
+                      <p>${name.length > 6 ? name.slice(0, 6) + '...' : name}</p>
+                      </div>
+                   </div>
+                  <div class="col d-flex">
+                  <i class="fa-solid fa-eye me-1 mt-2"></i> 
+                      <p class="mt-1">${total_view}</p>
+                   </div>
+                  <div class="col">
+                  <button class="rounded bg-secondary text-white" >
+                  <i class="fa fa-solid fa-arrow-right " onclick="authorDetails('${_id}')" style="text-decoration: none" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+                  </button>
+                  </div>
+              </div>
+          </div>
+            </small>
+            </div>
+  
+          </div>
+        </div>
+      </div>
+    </div>
+      `
+        thumbnailDetails.appendChild(createDiv);
+        spinnerLoading(false);
+    }
+    )
+}
 
 
 allCategories();
+thumbnailDetails();
